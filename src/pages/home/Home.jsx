9544c48c7fcf8e421b3e36bb104c447120b3bd1d
LordCarvel <viniciuseduardo0500@gmail.com>
2025-09-25 02:04:34 -0300

    +

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,33 @@
 import styles from "./Home.module.css";
 import Bottom from "../../components/bottom/Bottom";
 import Header from "../../components/header/Header";
+import edit from "../../../public/Edit.png"
 
-function Home () {
+function Home() {
 
- return (
-  <div className={styles.container}>
-    <Header />
-    <main>
-      <p>sla</p>
-    </main>
-    <Bottom />
-  </div>
- );
+  return (
+    <div className={styles.container}>
+      <Header />
+      <main>
+        <div className={styles.areasContainer}>
+          <h1 className={styles.title}>Áreas de entrega</h1>
+
+          <div className={styles.savedAreasContainer}>
+            <button>Criar nova área</button>
+
+            <div className={styles.areaCardContainer}>
+              <div className={styles.card}><p><span className={styles.color}></span>Penha 2 <span>8.00R$</span></p> <img src={edit} alt="" /></div>
+              <div className={styles.card}><p><span className={styles.color}></span>Penha 2 <span>8.00R$</span></p> <img src={edit} alt="" /></div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.mapContainer}>
+          <input type="text" />
+        </div>
+      </main>
+      <Bottom />
+    </div>
+  );
 }
 
 export default Home;
